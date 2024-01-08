1e950ec27660559a479a384592b7b1c07c60a524
Kevelaz <kevinvelazquez925@gmail.com>
2024-01-07 22:08:35 -0500

    added username

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,46 @@
-import User from "../models/user";
+import User from "../models/user.js";
 
 
 const registerUser = async (req,res) => {
   try {
-    const 
+    const {username} = req.body
+
+    const existingUser = await User.findOne({ username })
+
+    if(existingUser) {
+      return res.status(400).json({ message: "user already exists"})
+    }
+    const newUser = new User({ username })
+    await newUser.save()
+
+    res.status(201).json({ message: 'User registered successfully'})
+  } catch(error) {
+    console.error(error)
+    res.status(500).json({message:'internal server error'})
   }
 }
+
+
+const loginUser = async (req,res) => {
+  try {
+    const { username } = req.body
+
+    const user = await User.findOne({username})
+
+    if(!user) {
+      return res.status(404).json({message:'user not found'})
+    } 
+    res.status(200).json({message:'login successful', user})
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ message:'internal server error'})
+    }
+  }
+
+
+const userController = {
+  registerUser,
+  loginUser
+}
+
+export default userController
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import 'dotenv/config.js'
 import express from 'express'
 import cors from 'cors'
-import mainController from './controllers/mainControllers.js' 
 import { apiBaseURL, headers } from './config/index.js'
+import userController from './controllers/userController.js'
+import mainController from './controllers/mainControllers.js' 
+import favoriteController from './controllers/favoriteController.js'
+
+import userRouter from './routes/userRoutes.js'
 import router from './routes/routes.js'
+
 import mongoose from './connection.js'
 
 const app = express()
@@ -13,6 +18,7 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
+app.use('/user', userRouter)
 app.use('/main', router)
 
 
